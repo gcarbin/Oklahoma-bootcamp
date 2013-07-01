5d886e08ce91fc6ed5680455b4f98be2a3a9db8e
gcarbin <gregory.carbin@noaa.gov>
2013-07-01 14:29:47 -0500

    added module with functions

--- add_num.py
+++ add_num.py
@@ -1,9 +1,7 @@
+# updates include a new adding function
 
-#
-# this is a piece of code comments......
-def add_two(a,b):
-   return a+b
 def main():
+   print ("2+2+2 = ",add_three(2,2,2))
    print ("2 + 3 = ",add_two(2,3))
 if __name__ == "__main__":
      main()
